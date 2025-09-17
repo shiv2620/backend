6236dce95f12e38043b4fc788b0031920afd2776
shiv2620 <84431334+shiv2620@users.noreply.github.com>
2025-09-17 10:21:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -166,6 +166,12 @@ app.get('/api/getFullUrl/:id', (req, res) => {
     });
 });
 
+// ❌ Block root for public
+app.get('/', (req, res) => {
+  res.status(403).send("Access Denied");
+});
+
+
 // ------------------ START SERVER ------------------
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log('Backend running on port', PORT));
