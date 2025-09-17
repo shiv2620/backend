92957f06ce0f74a57de3b151343a657ebd06c28b
shiv2620 <84431334+shiv2620@users.noreply.github.com>
2025-09-17 17:37:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ app.get('/api/generate-pdf/:id', async (req, res) => {
     res.setHeader('Content-Type','application/pdf');
     doc.pipe(res);
 
-    const bgPath = path.resolve(__dirname,'public/certificate-bg.jpg');
+    const bgPath = path.resolve(__dirname,'certificate-bg.jpg');
     doc.image(bgPath,0,0,{width:doc.page.width,height:doc.page.height});
 
     doc.font('Times-Bold').fontSize(28).text(row.name,0,20,{align:'center'});
